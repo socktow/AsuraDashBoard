b6e2169ad92082aa03175f4ccdfd11f7d5557a00
socktow <mjssdn95@gmail.com>
2024-11-05 03:58:08 +0700

    add Guild and User Endpoint

---
+++ server/Router/DiscordRoutes/DiscordUser.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { Pool } = require("pg");
+const config = require("../../config.json");
+
+const pool = new Pool({
+  connectionString: config.databaseConfig.psqlConnectionString,
+});
+async function fetchFromDatabase(query, params, res, notFoundMessage) {
+  try {
+    const result = await pool.query(query, params);
+    if (result.rows.length) {
+      res.json(params ? result.rows[0] : result.rows);
+    } else {
+      res.status(404).send(notFoundMessage);
+    }
+  } catch (error) {
+    console.error("Database error:", error);
+    res.status(500).send("Error fetching data");
+  }
+}
+router.get("/users", (req, res) => {
+  fetchFromDatabase("SELECT * FROM public.discorduser ORDER BY id ASC", null, res, "No users found");
+});
+router.get("/users/:id", (req, res) => {
+  fetchFromDatabase("SELECT * FROM public.discorduser WHERE userid = $1", [req.params.id], res, "User not found");
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -19,20 +19,29 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Import routers and pass the Discord client instance
-const DiscordRoutes = require('./Router/DiscordRoutes/DiscordRoutes')(client);
-const MomoPaymentRouter = require('./Router/MomoPayment/MomoPayment');
-const ZalopaymentRouter = require('./Router/ZaloPayment/ZaloPayment');
-
 // Middleware setup
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Import routers and pass the Discord client instance
+const DiscordRoutes = require('./Router/DiscordRoutes/DiscordRoutes')(client);
+const MomoPaymentRouter = require('./Router/MomoPayment/MomoPayment');
+const ZalopaymentRouter = require('./Router/ZaloPayment/ZaloPayment');
+const DiscordUserRoutes = require('./Router/DiscordRoutes/DiscordUser');
+const DiscordGuildsRoutes = require('./Router/DiscordRoutes/DiscordGuilds')
 // Use routes
 app.use('/momo', MomoPaymentRouter);
 app.use('/zalo', ZalopaymentRouter);
 app.use(DiscordRoutes);
+app.use('/api', DiscordUserRoutes);
+app.use('/api', DiscordGuildsRoutes);
+
+// Error handler
+app.use((err, res) => {
+  console.error("An error occurred:", err.message);
+  res.status(500).send("Internal Server Error");
+});
 
 // Use port from config
 app.listen(config.port, () => {
